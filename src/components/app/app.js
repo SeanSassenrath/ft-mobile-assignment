46a415415f8ded2a8a20cb2d5e49743e13429670
SeanSassenrath <sean.sassenrath@gmail.com>
2016-11-24 01:10:14 -0800

    Add mobx and axios

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,18 @@
 import React, { Component, PropTypes } from 'react';
-import CSSModules from 'react-css-modules';
-import styles from './app.css';
 import Nav from '../nav/nav.js';
+import Devtools from 'mobx-react-devtools';
 import ProductPreview from '../product-preview/product-preview';
 import OptionGrid from '../option-grid/option-grid';
 import OptionRow from '../option-row/option-row';
+import CSSModules from 'react-css-modules';
+import styles from './app.css';
 
 class App extends Component {
 
   render() {
     return (
       <div>
+        <Devtools />
         <Nav />
         <ProductPreview />
         <OptionGrid />
